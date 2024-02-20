61987a84746c522566ae7f6bfe26e350e8a8212f
nzangi <nzangimuoki284@gmail.com>
2024-02-20 18:29:50 +0300

    blogpost-api update

--- users/views.py
+++ users/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view,permission_classes
 from users.serializers import RegisterUserSerializer,LoginUserSerializer
 
-
+# Server
+#AlwaysData
 # Create your views here.
 @api_view(['POST'])
 @permission_classes([])
-de
